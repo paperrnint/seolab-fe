0713fdf9d1ddb665e5bfc12e4fdb9a4c74f8a7b8
Eunji <eeunjiilee@gmail.com>
2025-07-01 08:05:10 +0900

    feat: add badge component and story

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,7 @@
+interface Props {
+  children: React.ReactNode;
+}
+
+export const Badge = ({ children }: Props) => {
+  return <div className="rounded-full bg-badge-bg text-badge-text text-[10px] px-2 py-1 w-fit">{children}</div>;
+};
