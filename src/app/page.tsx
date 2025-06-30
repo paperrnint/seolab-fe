38bb2cf343830bf2e06dabe4c116c7b82f4336b4
Eunji <eeunjiilee@gmail.com>
2025-06-30 12:47:22 +0900

    feat: add icon props to button component and shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import { Button } from '@/components/Button/Button';
-import { BsPlus } from 'react-icons/bs';
+import { NavItem } from '@/components/NavItem/NavItem';
+import { FaArrowRight, FaBoxArchive, FaHouse, FaStar } from 'react-icons/fa6';
 
 export default function Home() {
   return (
     <div className="p-2">
       <Button variant="primary">이어서 기록하기</Button>
       <Button variant="secondary">수정</Button>
-      <Button variant="accent">
-        <BsPlus size={20} />
-        이어서 기록하기
-      </Button>
+      <div className="w-62 flex flex-col gap-1">
+        <NavItem icon={<FaHouse className="text-secondary opacity-80" />}>홈</NavItem>
+        <NavItem icon={<FaStar className="text-secondary opacity-80" />}>즐겨찾기</NavItem>
+        <NavItem icon={<FaBoxArchive className="text-secondary opacity-80" />}>내 서랍</NavItem>
+        <NavItem>내 문장이 그렇게 이상한가요? · 김정선</NavItem>
+        <NavItem>여름은 오래 그곳에 남아 · 마쓰이에 마사시</NavItem>
+        <NavItem selected={true}>모순 · 양귀자</NavItem>
+        <Button variant="accent" rightIcon={<FaArrowRight className="opacity-100" />}>
+          새로운 책 기록하기
+        </Button>
+      </div>
     </div>
   );
 }
