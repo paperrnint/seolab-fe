b6caa6cb3d97298c4fbda872dfcbe57c954c5a5e
Eunji <eeunjiilee@gmail.com>
2025-07-03 11:09:04 +0900

    feat: add whitespace no wrap to badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -3,5 +3,9 @@ interface Props {
 }
 
 export const Badge = ({ children }: Props) => {
-  return <div className="rounded-full bg-badge-bg text-badge-text text-[10px] px-2 py-1 w-fit">{children}</div>;
+  return (
+    <div className="rounded-full bg-badge-bg text-badge-text text-[10px] px-2 py-1 w-fit whitespace-nowrap">
+      {children}
+    </div>
+  );
 };
