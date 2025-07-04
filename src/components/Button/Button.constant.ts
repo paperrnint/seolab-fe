75c3f06da4bd3cfb5bd8cbb3fa1e2ce4ec0090ab
Eunji <eeunjiilee@gmail.com>
2025-07-04 14:18:28 +0900

    feat: add button variant
    @todo: replace variant prop with size, color, width props

--- src/components/Button/Button.constant.ts
+++ src/components/Button/Button.constant.ts
@@ -3,6 +3,7 @@ export const variantColors = {
   secondary: 'border border-btn-primary text-primary',
   accent: 'bg-btn-accent text-text-btn',
   dropdown: 'bg-btn-primary text-text-btn',
+  big: 'bg-btn-primary text-text-btn',
 };
 
 export const variantShapes = {
@@ -10,4 +11,5 @@ export const variantShapes = {
   secondary: 'px-4 py-2 rounded-full',
   accent: 'w-full px-4 py-3 rounded-md',
   dropdown: 'min-w-20 px-3 py-2 text-xs rounded-full',
+  big: 'text-base leading-8 py-2 rounded-full border border-primary',
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,19 +2,34 @@ import { variantColors, variantShapes } from './Button.constant';
 
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  variant?: 'primary' | 'secondary' | 'accent' | 'dropdown';
+  variant?: 'primary' | 'secondary' | 'accent' | 'dropdown' | 'big';
+  isFull?: boolean;
+  isCenter?: boolean;
   leftIcon?: React.ReactNode;
   rightIcon?: React.ReactNode;
 }
 
-export const Button = ({ children, variant = 'primary', leftIcon, rightIcon, ...props }: Props) => {
+export const Button = ({
+  children,
+  variant = 'primary',
+  isFull = false,
+  isCenter = false,
+  leftIcon,
+  rightIcon,
+  ...props
+}: Props) => {
   const baseClasses = 'flex gap-1 items-center text-sm font-bold cursor-pointer hover:opacity-90 transition-opacity';
   const shadowClass = variant === 'accent' ? 'shadow-default' : '';
+  const fullClass = isFull ? 'w-full' : '';
+  const centerClass = isCenter ? 'text-center' : 'text-left';
 
   return (
-    <button className={`${baseClasses} ${variantColors[variant]} ${variantShapes[variant]} ${shadowClass}`} {...props}>
+    <button
+      className={`${baseClasses} ${variantColors[variant]} ${variantShapes[variant]} ${shadowClass} ${fullClass}`}
+      {...props}
+    >
       {leftIcon}
-      <div className="flex-1 text-left">{children}</div>
+      <div className={`flex-1 ${centerClass}`}>{children}</div>
       {rightIcon}
     </button>
   );
