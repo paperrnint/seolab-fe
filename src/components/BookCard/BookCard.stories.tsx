2edda33f83fc7d594ac60a923de1ebf39e3c62c9
Eunji <eeunjiilee@gmail.com>
2025-07-04 16:28:09 +0900

    fix: replace @storybook/nextjs-vite with @storybook/nextjs
    to fix svg issue

--- src/components/BookCard/BookCard.stories.tsx
+++ src/components/BookCard/BookCard.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import { BookCard } from './BookCard';
 
 const meta = {
--- src/components/QuoteInput/QuoteInput.stories.tsx
+++ src/components/QuoteInput/QuoteInput.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/nextjs-vite';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import { FaBoxArchive, FaHouse, FaStar } from 'react-icons/fa6';
 import { QuoteInput } from './QuoteInput';
 
