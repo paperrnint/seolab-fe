77e8f53dd67242d2714195e7878f8ded320ba906
Eunji <eeunjiilee@gmail.com>
2025-07-06 15:20:42 +0900

    fix: remove padding of login container

--- src/components/Login/LoginContainer/LoginContainer.tsx
+++ src/components/Login/LoginContainer/LoginContainer.tsx
@@ -3,5 +3,5 @@ interface Props {
 }
 
 export const LoginContainer = ({ children }: Props) => {
-  return <div className="w-full max-w-84 py-2 md:py-10 flex-shrink-0">{children}</div>;
+  return <div className="w-full max-w-84 flex-shrink-0">{children}</div>;
 };
