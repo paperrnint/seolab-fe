87570a6db4a74306daa769a37e42d4c5d0e35e1e
Eunji <eeunjiilee@gmail.com>
2025-07-08 10:19:24 +0900

    feat: add join schema and validations

---
+++ src/lib/validations/joinValidation.ts
@@ -0,0 +1,42 @@
+import { z } from 'zod/v4';
+
+import { ValidationText } from '@/types';
+
+import { JoinFormData } from '../schemas/joinSchema';
+
+export const getJoinValidations = (fieldName: keyof JoinFormData, value: string) => {
+  const validations: ValidationText[] = [];
+
+  if (fieldName === 'email') {
+    validations.push({
+      label: '올바른 이메일 형식',
+      isValid: z.email().safeParse(value).success,
+    });
+  }
+
+  if (fieldName === 'authCode') {
+    validations.push({
+      label: '인증번호 6자리',
+      isValid: value.length === 6,
+    });
+  }
+
+  if (fieldName === 'password') {
+    validations.push(
+      {
+        label: '8-20자',
+        isValid: value.length >= 8 && value.length <= 20,
+      },
+      {
+        label: '대소문자, 숫자, 특수문자 포함',
+        isValid: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/.test(value),
+      }
+    );
+  }
+
+  if (fieldName === 'confirmPassword') {
+    // component 에서 추가
+  }
+
+  return validations;
+};
