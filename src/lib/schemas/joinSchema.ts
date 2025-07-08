3db0be5c7542bb975f4bdff4fe9677c1b7ff53a1
Eunji <eeunjiilee@gmail.com>
2025-07-08 10:19:24 +0900

    feat: add join schema and validations

---
+++ src/lib/schemas/joinSchema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod/v4';
+
+export const joinSchema = z
+  .object({
+    email: z.email('올바른 이메일 형식'),
+    authCode: z.string().length(6, '인증번호 6자리'),
+    password: z
+      .string()
+      .min(8, '최소 8자 이상')
+      .max(20, '최대 20자 이하')
+      .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).*$/, '대소문자, 숫자 ,특수문자 포함'),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: '비밀번호와 일치',
+    path: ['confirmPassword'],
+  });
+
+export type JoinFormData = z.infer<typeof joinSchema>;
