09ca736b09563e13ec253e9619ac5c1d2c1ac646
Eunji <eeunjiilee@gmail.com>
2025-07-10 07:50:48 +0900

    feat: add shadow option to card and book cover

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,14 @@
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
+  hasShadow?: boolean;
 }
 
-export const Card = ({ children, ...props }: Props) => {
+export const Card = ({ children, hasShadow = true, ...props }: Props) => {
   return (
-    <div className="border border-border shadow-default rounded-lg px-4 py-3 bg-white" {...props}>
+    <div
+      className={`border border-border ${hasShadow ? 'shadow-default' : ''} rounded-lg px-4 py-3 bg-white w-full`}
+      {...props}
+    >
       {children}
     </div>
   );
