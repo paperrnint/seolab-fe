18e8d90130ee1a87fd81a6c6da84da4d7b21e1dc
Eunji <eeunjiilee@gmail.com>
2025-07-09 20:34:34 +0900

    chore: add storybook viewport

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from '@storybook/nextjs';
+import { INITIAL_VIEWPORTS } from 'storybook/viewport';
 import '../src/app/globals.css';
 
 const preview: Preview = {
@@ -9,7 +10,9 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-
+    viewport: {
+      options: INITIAL_VIEWPORTS,
+    },
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
