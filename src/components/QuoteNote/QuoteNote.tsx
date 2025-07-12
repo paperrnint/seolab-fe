67484498eda9b30fea88c3607550eeb7a1b79ad3
Eunji <eeunjiilee@gmail.com>
2025-07-12 13:29:54 +0900

    feat: apply text-justify to quote components

--- src/components/QuoteNote/QuoteNote.tsx
+++ src/components/QuoteNote/QuoteNote.tsx
@@ -12,7 +12,7 @@ export const QuoteNote = ({ quote, page, line }: Props) => {
   return (
     <Card hasShadow={true}>
       <div className="h-full flex flex-col justify-between gap-1">
-        <p className={`leading-7 underline-dotted ${line ? quoteConfig[line] : ''}`}>{quote}</p>
+        <p className={`leading-7 text-justify underline-dotted ${line ? quoteConfig[line] : ''}`}>{quote}</p>
         <p className="leading-6 text-secondary text-right text-xs">{page}p</p>
       </div>
     </Card>
--- src/components/QuoteText/QuoteText.tsx
+++ src/components/QuoteText/QuoteText.tsx
@@ -9,7 +9,7 @@ export const QuoteText = ({ page, quote, isFavorite = false, showPage = true }: 
   return (
     <div className="flex gap-4 py-2 px-4 lg:pl-5 lg:pr-3 leading-7 border border-transparent">
       {showPage && <div className="w-12">{page}p</div>}
-      <div className={`flex-1 ${isFavorite && 'underline-dotted'}`}>{quote}</div>
+      <div className={`flex-1 text-justify ${isFavorite && 'underline-dotted'}`}>{quote}</div>
     </div>
   );
 };
