4afacf98c62dad89a22f1e95e796197616f0741a
Eunji <eeunjiilee@gmail.com>
2025-07-12 13:31:52 +0900

    feat: add navigation to continue button

--- src/components/NowBookHeader/NowBookHeader.tsx
+++ src/components/NowBookHeader/NowBookHeader.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
 import { Badge } from '../Badge/Badge';
 import { Button } from '../Button/Button';
 import { Title } from '../Title/Title';
@@ -9,6 +13,14 @@ interface Props {
 }
 
 export const NowBookHeader = ({ title, dateDiff, count }: Props) => {
+  const BOOK_ID = 'abcd';
+  const router = useRouter();
+
+  const onClick = () => {
+    router.push(`/book/${BOOK_ID}`);
+    // @todo: start with 'edit' mode
+  };
+
   return (
     <div className="pb-4">
       <div className="flex gap-2 flex-col md:flex-row items-start md:items-center">
@@ -25,7 +37,7 @@ export const NowBookHeader = ({ title, dateDiff, count }: Props) => {
           </div>
           {/* Action */}
           <div>
-            <Button>이어서 기록하기</Button>
+            <Button onClick={onClick}>이어서 기록하기</Button>
           </div>
         </div>
       </div>
