6a7771ef9ffeca00d91fc60fdc66bad4e5305605
Eunji <eeunjiilee@gmail.com>
2025-07-13 16:00:33 +0900

    feat: update button and txt style

--- src/components/Txt/Txt.tsx
+++ src/components/Txt/Txt.tsx
@@ -7,8 +7,8 @@ interface Props {
 export const Txt = ({ children, variant = 'base', truncated = false }: Props) => {
   const classes = {
     base: 'text-primary text-sm leading-7',
-    caption: 'text-sm font-bold text-subtle',
-    bold: 'text-sm font-bold',
+    caption: 'text-sm font-bold text-subtle leading-6',
+    bold: 'text-primary text-sm font-bold',
     muted: 'text-xs text-text-muted',
   };
 
