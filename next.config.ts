df9b4bebd61f7fbacefda5a72ffd879fdec490de
Eunji <eeunjiilee@gmail.com>
2025-07-18 09:32:35 +0900

    feat: add integrated book search functionality

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['search1.kakaocdn.net'],
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
