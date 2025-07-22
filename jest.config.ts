e9559b0cc81d5745576ddf625fde3e38b2f809bd
Eunji <eeunjiilee@gmail.com>
2025-07-22 19:59:00 +0900

    chore: setup jest, testing library, and msw for testing

---
+++ jest.config.ts
@@ -0,0 +1,36 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFiles: ['whatwg-fetch'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
+  testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
+
+export default createJestConfig(config);
