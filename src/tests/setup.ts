f74933b4238e6ca0e0c937671cec4136199b393e
Eunji <eeunjiilee@gmail.com>
2025-07-22 19:59:00 +0900

    chore: setup jest, testing library, and msw for testing

---
+++ src/tests/setup.ts
@@ -0,0 +1,20 @@
+import '@testing-library/jest-dom';
+import { server } from './__mocks__/server';
+
+// MSW 서버 설정
+beforeAll(() => {
+  server.listen({
+    // 처리되지 않은 요청에 대해 경고 표시
+    onUnhandledRequest: 'warn',
+  });
+});
+
+afterEach(() => {
+  // 각 테스트 후 핸들러 리셋
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // 테스트 완료 후 서버 종료
+  server.close();
+});
