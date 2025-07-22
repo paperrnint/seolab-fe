740360a778ebb8c7dd78a41a81d3588fc1f682df
Eunji <eeunjiilee@gmail.com>
2025-07-22 19:59:52 +0900

    test: add testid to Backdrop component

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -5,7 +5,8 @@ interface Props {
 export const Backdrop = ({ onClick }: Props) => {
   return (
     <div
-      className="fixed inset-0 bg-black/30 backdrop-blur-xs transition-all duration-300 ease-out"
+      data-testid="modal-backdrop"
+      className="fixed inset-0 bg-black/30 backdrop-blur-xs transition-all duration-300 ease-out z-10"
       onClick={onClick}
     />
   );
