7ad262c96723a4911477235d00e48460beb898c8
Eunji <eeunjiilee@gmail.com>
2025-07-22 18:17:25 +0900

    fix: resolve backdrop click not closing modal

--- src/components/Modal/ModalAction/ModalAction.tsx
+++ src/components/Modal/ModalAction/ModalAction.tsx
@@ -1,13 +1,7 @@
-import { FocusTrap } from 'focus-trap-react';
-
 interface Props {
   children: React.ReactNode;
 }
 
 export const ModalAction = ({ children }: Props) => {
-  return (
-    <FocusTrap>
-      <div className={`flex w-full gap-2`}>{children}</div>
-    </FocusTrap>
-  );
+  return <div className={`flex w-full gap-2`}>{children}</div>;
 };
