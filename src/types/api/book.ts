02aa589bff75c047d3b7dbaad2cb521a2914fd5e
Eunji <eeunjiilee@gmail.com>
2025-07-26 22:25:28 +0900

    feat: separate book server and client types with mappers

--- src/types/api/book.ts
+++ src/types/api/book.ts
@@ -1,22 +1,65 @@
-export interface SearchRequest {
+type Book = {
+  title: string;
+  isbn: string;
+  authors: string[];
+  publisher: string;
+  publishedDate: string;
+  translators: string[];
+  thumbnail: string;
+};
+
+export type BookSearchApiItem = Book & {
+  contents: string;
+};
+
+export type BookListApiItem = {
+  userBookId: string;
+  book: BookSearchApiItem;
+  startDate: string;
+  endDate: string | null;
+  isFavorite: boolean;
+  isReading: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+// GET api/books/search
+export type BookSearchRequest = {
   query: string;
   page?: number;
   size?: number;
-}
+};
 
-export interface SearchBook {
-  title: string;
-  contents: string;
-  isbn: string;
-  publishedDate: string;
-  authors: string[];
-  publisher: string;
-  translators: string[];
-  thumbnail: string;
-}
-
-export interface SearchResponse {
-  books: SearchBook[];
+export type BookSearchResponse = {
+  books: BookSearchApiItem[];
   totalCount: number;
   isEnd: boolean;
-}
+};
+
+// POST api/books
+export type CreateBookRequest = BookSearchApiItem;
+
+export type CreateBookResponse = {
+  userBookId: string;
+};
+
+// GET api/books
+export type GetBooksRequest = {
+  isFavorite?: boolean;
+  isReading?: boolean;
+};
+
+export type GetBooksResponse = BookListApiItem[];
+
+// GET api/books/{id}
+export type GetBookDetailResponse = {
+  userBookId: string;
+  book: Book;
+  startDate: string;
+  endDate: string | null;
+  isFavorite: boolean;
+  isReading: boolean;
+  createdAt: string;
+  updatedAt: string;
+  // quotes:
+};
--- src/types/domain/book.ts
+++ src/types/domain/book.ts
@@ -2,3 +2,29 @@ export type Quote = {
   page: number;
   quote: string;
 };
+
+type Book = {
+  title: string;
+  isbn: string;
+  author: string;
+  publisher: string;
+  publishedDate: string;
+  translator: string;
+  thumbnail: string;
+};
+
+export type BookSearchItem = Book & {
+  description: string;
+};
+
+export type BookDetailItem = Book & {
+  id: string;
+  startDate: string;
+  endDate: string | null;
+  isReading: boolean;
+  isFavorite: boolean;
+  // quotes: Quote[];
+  // quoteCount: number;
+};
+
+export type BookCardItem = Omit<BookDetailItem, 'isbn' | 'publisher' | 'publishedDate' | 'translator'>;
