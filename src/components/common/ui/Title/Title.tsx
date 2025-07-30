88be5b0a2bd9499d1ab2541244e479fd59431598
Eunji <eeunjiilee@gmail.com>
2025-07-30 11:12:56 +0900

    refactor: add error variant to Title component for search errors

--- src/components/common/ui/Title/Title.tsx
+++ src/components/common/ui/Title/Title.tsx
@@ -1,12 +1,30 @@
+import { FaCircleExclamation } from 'react-icons/fa6';
+
 interface Props extends React.HTMLAttributes<HTMLHeadingElement> {
   children: React.ReactNode;
+  variant?: 'base' | 'error';
 }
 
 // @todo: variation 추가
-export const Title = ({ children, ...props }: Props) => {
-  return (
-    <h2 className="text-xl leading-7 font-bold" {...props}>
-      {children}
-    </h2>
-  );
+export const Title = ({ children, variant = 'base', ...props }: Props) => {
+  if (variant === 'base') {
+    return (
+      <h2 className="text-xl leading-7 font-bold" {...props}>
+        {children}
+      </h2>
+    );
+  }
+
+  if (variant === 'error') {
+    return (
+      <div className="w-full my-2 text-center">
+        <div className="flex flex-col gap-3 w-full">
+          <div className="flex justify-center items-center text-2xl text-subtle">
+            <FaCircleExclamation />
+          </div>
+          <h2 className="text-primary text-sm font-bold">{children}</h2>
+        </div>
+      </div>
+    );
+  }
 };
