2baf7bae73cd6e038714aa409f1fa198c02ec65d
Eunji <eeunjiilee@gmail.com>
2025-07-30 11:16:39 +0900

    style: decrease vertical spacing in QuoteText

--- src/components/book/QuoteText/QuoteText.tsx
+++ src/components/book/QuoteText/QuoteText.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const QuoteText = ({ page, text, isFavorite = false, showPage = true }: Props) => {
   return (
-    <div className="flex gap-4 py-2 px-4 lg:pl-5 lg:pr-3 leading-7 border border-transparent">
+    <div className="flex gap-4 py-1 px-4 lg:pl-5 lg:pr-3 leading-7 border border-transparent">
       {showPage && <div className="w-12">{page !== null && `${page}p`}</div>}
       <div className={`flex-1 text-justify ${isFavorite && 'underline-dotted'}`}>{text}</div>
     </div>
