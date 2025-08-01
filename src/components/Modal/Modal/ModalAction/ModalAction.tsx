eb5f22d8ad21e8237e7ef84a4987bba48c1bee4b
Eunji <eeunjiilee@gmail.com>
2025-08-01 19:40:28 +0900

    fix: remove uppercase Modal folder

--- src/components/Modal/Modal/ModalAction/ModalAction.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  children: React.ReactNode;
-}
-
-export const ModalAction = ({ children }: Props) => {
-  return <div className={`flex w-full gap-2 justify-end`}>{children}</div>;
-};
--- src/components/Modal/Modal/ModalContent/ModalContent.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  children: React.ReactNode;
-}
-
-export const ModalContent = ({ children }: Props) => {
-  return <div className={`mb-6 text-center whitespace-pre-wrap`}>{children}</div>;
-};
