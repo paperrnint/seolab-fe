d2d2565c6decfeee196013bfdc3fbea7605aac90
Eunji <eeunjiilee@gmail.com>
2025-08-03 13:01:14 +0900

    refactor: remove unused util date function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,27 +3,6 @@ export * from './date';
 
 import { ERROR_MESSAGES, ErrorType } from '@/constants';
 
-/**
- * 날짜 차이를 구하는 유틸 함수
- * @param startAt xxxx-xx-xx 형태
- * @param endAt xxxx-xx-xx 형태
- * @returns
- */
-export const getDaysDiff = (startAt: string, endAt?: string) => {
-  const start = new Date(startAt);
-  const end = endAt ? new Date(endAt) : new Date();
-
-  const diffTime = end.getTime() - start.getTime();
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
-  return diffDays + 1;
-};
-
-export const convertDateText = (date: string) => {
-  const [y, m, d] = date.split('-');
-  return `${+m}월 ${+d}일`;
-};
-
 export const getTextsByLine = (text: string) => {
   const normalizedText = text.replace(/\\n/g, '\n');
   return normalizedText.split('\n');
