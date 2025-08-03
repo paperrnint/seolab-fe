a3a7eb789aec9fb53354a03f9435b0a412a5e788
Eunji <eeunjiilee@gmail.com>
2025-08-03 16:09:23 +0900

    feat: add HoverSwiper to main page and update QuoteNote styling

--- src/components/book/QuoteNote/QuoteNote.tsx
+++ src/components/book/QuoteNote/QuoteNote.tsx
@@ -5,15 +5,18 @@ import { quoteConfig } from './QuoteNote.constant';
 interface Props {
   text: string;
   page: number | null;
+  bookTitle: string;
+  author: string;
   line?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-export const QuoteNote = ({ text, page, line }: Props) => {
+export const QuoteNote = ({ text, page, bookTitle, author, line }: Props) => {
+  const pageText = !!page ? `${bookTitle} · ${author}, ${page}p` : `${bookTitle} · ${author}`;
   return (
-    <Card hasShadow={true}>
-      <div className="h-full flex flex-col justify-between gap-1">
-        <p className={`leading-7 text-justify underline-dotted ${line ? quoteConfig[line] : ''}`}>{text}</p>
-        {page && <p className="leading-6 text-secondary text-right text-xs">{page}p</p>}
+    <Card hasShadow={false}>
+      <div className="h-full flex flex-col justify-between gap-2">
+        <p className={`leading-7 text-justify dotted-lines-bg ${line ? quoteConfig[line] : ''}`}>{text}</p>
+        <p className="leading-5 text-secondary text-right text-[10px]">{pageText}</p>
       </div>
     </Card>
   );
