c92915806a6033f48d0a1f30b2855cb5781e233c
Eunji <eeunjiilee@gmail.com>
2025-08-04 18:38:04 +0900

    feat: add sm variant to DropdownSelectableItem

--- src/components/common/ui/Dropdown/DropdownSelectableItem/DropdownSelectableItem.tsx
+++ src/components/common/ui/Dropdown/DropdownSelectableItem/DropdownSelectableItem.tsx
@@ -4,34 +4,44 @@ import { DropdownOption } from '@/types/ui/common';
 
 import { useDropdown } from '../@context/DropdownContext';
 
-interface Props {
+import { selectableItemConfig, SelectableItemVariant } from './DropdownSelectableItem.constant';
+
+interface Props<T extends DropdownOption = DropdownOption> {
   children: React.ReactNode;
+  variant?: SelectableItemVariant;
   value: string;
   icon?: React.ReactNode;
-  onSelect?: (options: DropdownOption) => void;
+  onSelect?: (option: T) => void;
 }
 
-export const DropdownSelectableItem = ({ children, value, icon, onSelect }: Props) => {
+export const DropdownSelectableItem = <T extends DropdownOption = DropdownOption>({
+  children,
+  variant = 'default',
+  value,
+  icon,
+  onSelect,
+}: Props<T>) => {
   const { onClose } = useDropdown();
   const label = typeof children === 'string' ? children : value;
+  const style = selectableItemConfig[variant];
 
   const onClick = () => {
-    const option = { value, label };
+    const option = { value, label } as T;
     onSelect?.(option);
     onClose();
   };
 
   return (
     <button
-      className="
+      className={`
         flex gap-2 items-center 
-        w-full px-3 py-2 
-        cursor-pointer hover:bg-bg-hover
-      "
+        w-full ${style.spacing}
+        cursor-pointer hover:bg-bg-hover/60
+      `}
       onClick={onClick}
     >
       {icon && icon}
-      <div className="text-primary text-xs">{children}</div>
+      <div className={`text-primary ${style.text}`}>{children}</div>
     </button>
   );
 };
